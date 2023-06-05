2023be3b8347edfecd2a7e51ebf44843a0d236af
Наталия <pedorg129@gmail.com>
2023-06-05 22:36:47 +0300

    fix layout

--- src/route/index.js
+++ src/route/index.js
@@ -269,287 +269,108 @@ router.get('/work', function (req, res) {
   /
   // ================================================================
 
-  //              ↙ тут вводимо шлях (PATH) до сторінки
-  router.get('/person', function (req, res) {
-    //             ↙ cюди вводимо назву файлу з сontainer
-    res.render('person', {
-      layout: 'person',
-      person: {
-        name: 'Emma Johnson',
-        age: 32,
-        gender: 'Female',
-        address: {
-          street: '123 Main St',
-          city: 'New York',
-          state: 'NY',
-          zip: '10001',
-          country: 'USA',
+  //              
+
+
+  //======================================================
+  // router.get Створює нам один ентпоїнт
+
+  router.get('/car', function (req, res) {
+    // res.render генерує нам HTML сторінку
+
+    //            ↙ cюди вводимо назву файлу з сontainer
+    res.render('car', {
+      make: 'Toyota',
+      model: 'Camry',
+      year: 2022,
+      color: 'silver',
+      features: {
+        interior: {
+          seats: {
+            material: 'leather',
+            color: 'black',
+            heated: true,
+            ventilated: true,
+          },
+          dashboard: {
+            material: 'plastic',
+            color: 'black',
+            display: {
+              type: 'LCD',
+              size: 10.1,
+              resolution: '1280x720',
+              touchscreen: true,
+            },
+          },
+          audio: {
+            system: 'JBL',
+            speakers: 8,
+            subwoofer: true,
+            bluetooth: true,
+            USB: true,
+          },
         },
-        education: [
-          {
-            degree: 'Bachelor of Science',
-            major: 'Computer Science',
-            university:
-              'Massachusetts Institute of Technology',
-            graduationYear: 2012,
+        exterior: {
+          wheels: {
+            size: 18,
+            type: 'alloy',
+            color: 'silver',
           },
-        ],
-        workExperience: [
-          {
-            company: 'Google',
-            title: 'Software Engineer',
-            startDate: '2012-06-01',
-            endDate: '2016-12-31',
-            responsibilities: [
-              'Developed new features for Google Maps',
-              'Worked on improving search algorithms',
-            ],
-            year_founded: 1990,
-            industry: 'Technology',
-            employees: [
-              {
-                name: 'John Smith',
-                position: 'CEO',
-                department: 'Executive',
-                projects: [
-                  {
-                    name: 'Project Alpha',
-                    description:
-                      'Developing new software platform',
-                    status: 'In Progress',
-                    teams: [
-                      {
-                        team_name: 'Awesome Team',
-                        team_leader: {
-                          name: 'John Smith',
-                          title: 'Team Leader',
-                          email: 'john.smith@example.com',
-                        },
-                        team_members: [
-                          {
-                            name: 'Alice Johnson',
-                            title: 'Software Engineer',
-                            email:
-                              'alice.johnson@example.com',
-                            skills: ['Java', 'Python', 'SQL'],
-                            projects: [
-                              {
-                                name: 'Project A',
-                                description:
-                                  'Lorem ipsum dolor sit amet',
-                                technologies: [
-                                  'Java',
-                                  'Spring Framework',
-                                ],
-                                team_members: [
-                                  {
-                                    name: 'Bob Lee',
-                                    title:
-                                      'Software Engineer',
-                                  },
-                                  {
-                                    name: 'Cindy Chen',
-                                    title: 'UI Designer',
-                                  },
-                                ],
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
+          headlights: {
+            type: 'LED',
+            brightness: 'high',
+            automatic: true,
           },
-        ],
+          sunroof: {
+            type: 'panoramic',
+            size: 'large',
+            automatic: true,
+          },
+        },
+        safety: {
+          airbags: {
+            front: 2,
+            side: 2,
+            knee: 2,
+            rear: 2,
+          },
+          assistance: {
+            blind_spot_monitoring: true,
+            rear_cross_traffic_alert: true,
+            lane_departure_warning: true,
+            adaptive_cruise_control: true,
+            collision_warning: true,
+          },
+        },
+      },
+      engine: {
+        type: 'gasoline',
+        displacement: 2.5,
+        horsepower: 206,
+        torque: 186,
+        transmission: {
+          type: 'automatic',
+          gears: 8,
+        },
+      },
+      fuel_economy: {
+        city: 28,
+        highway: 39,
+        combined: 32,
+      },
+      price: {
+        base: 25900,
+        destination: 995,
+        options: {
+          navigation: 1200,
+          moonroof: 800,
+          premium_paint: 595,
+        },
+        total: 28990,
       },
     })
+    //                  ↑↑ сюди вводимо JSON дані
   })
-
-// ================================================================
-
-
-
-router.get('/js', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('js', {
-    name: 'JavaScript',
-    description:
-      'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
-    history: {
-      year: 1995,
-      founder: 'Brendan Eich',
-    },
-    types: [
-      'Number',
-      'String',
-      'Boolean',
-      'Null',
-      'Undefined',
-      'Symbol',
-      'Object',
-    ],
-    syntax: {
-      variables: ['var', 'let', 'const'],
-      functions: ['function'],
-      conditionals: ['if', 'else', 'switch'],
-      loops: ['for', 'while', 'do-while'],
-      classes: ['class'],
-    },
-    features: [
-      'First-class functions',
-      'Closures',
-      'Dynamic typing',
-      'Prototype-based inheritance',
-      'Asynchronous programming with Promises and async/await',
-      'Modules with import/export statements',
-    ],
-    libraries: [
-      'jQuery',
-      'React',
-      'Angular',
-      'Vue',
-      'Node.js',
-      'Express.js',
-      'Lodash',
-      'Moment.js',
-    ],
-    tools: [
-      'Babel',
-      'Webpack',
-      'ESLint',
-      'Jest',
-      'Mocha',
-      'Chai',
-      'Selenium',
-      'Cypress',
-    ],
-    community: [
-      {
-        name: 'Stack Overflow',
-        type: 'forum',
-      },
-      {
-        name: 'JavaScript Weekly',
-        type: 'newsletter',
-      },
-      {
-        name: 'The Changelog',
-        type: 'podcast',
-      },
-      {
-        name: 'CSS-Tricks',
-        type: 'blog',
-      },
-    ],
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
-
-
-//======================================================
-// router.get Створює нам один ентпоїнт
-
-router.get('/car', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('car', {
-    make: 'Toyota',
-    model: 'Camry',
-    year: 2022,
-    color: 'silver',
-    features: {
-      interior: {
-        seats: {
-          material: 'leather',
-          color: 'black',
-          heated: true,
-          ventilated: true,
-        },
-        dashboard: {
-          material: 'plastic',
-          color: 'black',
-          display: {
-            type: 'LCD',
-            size: 10.1,
-            resolution: '1280x720',
-            touchscreen: true,
-          },
-        },
-        audio: {
-          system: 'JBL',
-          speakers: 8,
-          subwoofer: true,
-          bluetooth: true,
-          USB: true,
-        },
-      },
-      exterior: {
-        wheels: {
-          size: 18,
-          type: 'alloy',
-          color: 'silver',
-        },
-        headlights: {
-          type: 'LED',
-          brightness: 'high',
-          automatic: true,
-        },
-        sunroof: {
-          type: 'panoramic',
-          size: 'large',
-          automatic: true,
-        },
-      },
-      safety: {
-        airbags: {
-          front: 2,
-          side: 2,
-          knee: 2,
-          rear: 2,
-        },
-        assistance: {
-          blind_spot_monitoring: true,
-          rear_cross_traffic_alert: true,
-          lane_departure_warning: true,
-          adaptive_cruise_control: true,
-          collision_warning: true,
-        },
-      },
-    },
-    engine: {
-      type: 'gasoline',
-      displacement: 2.5,
-      horsepower: 206,
-      torque: 186,
-      transmission: {
-        type: 'automatic',
-        gears: 8,
-      },
-    },
-    fuel_economy: {
-      city: 28,
-      highway: 39,
-      combined: 32,
-    },
-    price: {
-      base: 25900,
-      destination: 995,
-      options: {
-        navigation: 1200,
-        moonroof: 800,
-        premium_paint: 595,
-      },
-      total: 28990,
-    },
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
 //======================================================
 
 
