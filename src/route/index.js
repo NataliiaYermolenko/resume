a5bf132cb9b0f12ecd8befe43760a72ddf6f81f5
Наталия <pedorg129@gmail.com>
2023-06-05 23:15:02 +0300

    fix layout

--- src/route/index.js
+++ src/route/index.js
@@ -269,276 +269,94 @@ router.get('/work', function (req, res) {
   /
   // ================================================================
 
-  //              ↙ тут вводимо шлях (PATH) до сторінки
-  router.get('/person', function (req, res) {
-    //             ↙ cюди вводимо назву файлу з сontainer
-    res.render('person', {
-      layout: 'person',
-      person: {
-        name: 'Emma Johnson',
-        age: 32,
-        gender: 'Female',
-        address: {
-          street: '123 Main St',
-          city: 'New York',
-          state: 'NY',
-          zip: '10001',
-          country: 'USA',
+  //            
+  // router.get Створює нам один ентпоїнт
+
+  router.get('/mac', function (req, res) {
+    // res.render генерує нам HTML сторінку
+
+    //            ↙ cюди вводимо назву файлу з сontainer
+    res.render('mac', {
+      name: 'Apple MacBook Pro',
+      description: 'The ultimate notebook for power users',
+      price: 1999.99,
+      category: 'Computers & Tablets',
+      features: {
+        processor: {
+          brand: 'Intel',
+          model: 'Core i9',
+          speed: '2.3 GHz',
+          cores: 8,
+          cache: '16 MB',
         },
-        education: [
+        memory: {
+          type: 'DDR4',
+          size: '32 GB',
+          speed: '2666 MHz',
+        },
+        storage: {
+          type: 'SSD',
+          size: '1 TB',
+        },
+        display: {
+          size: '16 inches',
+          resolution: '3072x1920',
+          technology: 'Retina',
+          color_space: 'P3',
+          brightness: '500 nits',
+          refresh_rate: '60 Hz',
+        },
+        graphics: {
+          brand: 'AMD',
+          model: 'Radeon Pro 5500M',
+          memory: '4 GB GDDR6',
+        },
+        ports: [
           {
-            degree: 'Bachelor of Science',
-            major: 'Computer Science',
-            university:
-              'Massachusetts Institute of Technology',
-            graduationYear: 2012,
+            type: 'Thunderbolt 3',
+            count: 4,
+            features: [
+              'Charging',
+              'DisplayPort',
+              'Thunderbolt (up to 40 Gbps)',
+              'USB 3.1 Gen 2 (up to 10 Gbps)',
+            ],
+          },
+          {
+            type: 'HDMI',
+            count: 1,
           },
         ],
-        workExperience: [
+        battery: {
+          type: 'Lithium Polymer',
+          capacity: '100 Wh',
+          life: 'Up to 11 hours',
+        },
+        weight: '4.3 pounds',
+        dimensions: {
+          height: '0.64 inch',
+          width: '14.09 inches',
+          depth: '9.68 inches',
+        },
+        operating_system: 'macOS',
+        accessories: [
           {
-            company: 'Google',
-            title: 'Software Engineer',
-            startDate: '2012-06-01',
-            endDate: '2016-12-31',
-            responsibilities: [
-              'Developed new features for Google Maps',
-              'Worked on improving search algorithms',
-            ],
-            year_founded: 1990,
-            industry: 'Technology',
-            employees: [
-              {
-                name: 'John Smith',
-                position: 'CEO',
-                department: 'Executive',
-                projects: [
-                  {
-                    name: 'Project Alpha',
-                    description:
-                      'Developing new software platform',
-                    status: 'In Progress',
-                    teams: [
-                      {
-                        team_name: 'Awesome Team',
-                        team_leader: {
-                          name: 'John Smith',
-                          title: 'Team Leader',
-                          email: 'john.smith@example.com',
-                        },
-                        team_members: [
-                          {
-                            name: 'Alice Johnson',
-                            title: 'Software Engineer',
-                            email:
-                              'alice.johnson@example.com',
-                            skills: ['Java', 'Python', 'SQL'],
-                            projects: [
-                              {
-                                name: 'Project A',
-                                description:
-                                  'Lorem ipsum dolor sit amet',
-                                technologies: [
-                                  'Java',
-                                  'Spring Framework',
-                                ],
-                                team_members: [
-                                  {
-                                    name: 'Bob Lee',
-                                    title:
-                                      'Software Engineer',
-                                  },
-                                  {
-                                    name: 'Cindy Chen',
-                                    title: 'UI Designer',
-                                  },
-                                ],
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
+            name: 'Apple Magic Keyboard',
+            price: 99.99,
+          },
+          {
+            name: 'Apple Magic Mouse 2',
+            price: 79.99,
+          },
+          {
+            name: 'USB-C to USB Adapter',
+            price: 19.99,
           },
         ],
       },
     })
+    //                  ↑↑ сюди вводимо JSON дані
   })
-
-// ================================================================
-
-
-
-router.get('/js', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('js', {
-    name: 'JavaScript',
-    description:
-      'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
-    history: {
-      year: 1995,
-      founder: 'Brendan Eich',
-    },
-    types: [
-      'Number',
-      'String',
-      'Boolean',
-      'Null',
-      'Undefined',
-      'Symbol',
-      'Object',
-    ],
-    syntax: {
-      variables: ['var', 'let', 'const'],
-      functions: ['function'],
-      conditionals: ['if', 'else', 'switch'],
-      loops: ['for', 'while', 'do-while'],
-      classes: ['class'],
-    },
-    features: [
-      'First-class functions',
-      'Closures',
-      'Dynamic typing',
-      'Prototype-based inheritance',
-      'Asynchronous programming with Promises and async/await',
-      'Modules with import/export statements',
-    ],
-    libraries: [
-      'jQuery',
-      'React',
-      'Angular',
-      'Vue',
-      'Node.js',
-      'Express.js',
-      'Lodash',
-      'Moment.js',
-    ],
-    tools: [
-      'Babel',
-      'Webpack',
-      'ESLint',
-      'Jest',
-      'Mocha',
-      'Chai',
-      'Selenium',
-      'Cypress',
-    ],
-    community: [
-      {
-        name: 'Stack Overflow',
-        type: 'forum',
-      },
-      {
-        name: 'JavaScript Weekly',
-        type: 'newsletter',
-      },
-      {
-        name: 'The Changelog',
-        type: 'podcast',
-      },
-      {
-        name: 'CSS-Tricks',
-        type: 'blog',
-      },
-    ],
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
-
-//==================================================================
-
-// router.get Створює нам один ентпоїнт
-
-router.get('/mac', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('mac', {
-    name: 'Apple MacBook Pro',
-    description: 'The ultimate notebook for power users',
-    price: 1999.99,
-    category: 'Computers & Tablets',
-    features: {
-      processor: {
-        brand: 'Intel',
-        model: 'Core i9',
-        speed: '2.3 GHz',
-        cores: 8,
-        cache: '16 MB',
-      },
-      memory: {
-        type: 'DDR4',
-        size: '32 GB',
-        speed: '2666 MHz',
-      },
-      storage: {
-        type: 'SSD',
-        size: '1 TB',
-      },
-      display: {
-        size: '16 inches',
-        resolution: '3072x1920',
-        technology: 'Retina',
-        color_space: 'P3',
-        brightness: '500 nits',
-        refresh_rate: '60 Hz',
-      },
-      graphics: {
-        brand: 'AMD',
-        model: 'Radeon Pro 5500M',
-        memory: '4 GB GDDR6',
-      },
-      ports: [
-        {
-          type: 'Thunderbolt 3',
-          count: 4,
-          features: [
-            'Charging',
-            'DisplayPort',
-            'Thunderbolt (up to 40 Gbps)',
-            'USB 3.1 Gen 2 (up to 10 Gbps)',
-          ],
-        },
-        {
-          type: 'HDMI',
-          count: 1,
-        },
-      ],
-      battery: {
-        type: 'Lithium Polymer',
-        capacity: '100 Wh',
-        life: 'Up to 11 hours',
-      },
-      weight: '4.3 pounds',
-      dimensions: {
-        height: '0.64 inch',
-        width: '14.09 inches',
-        depth: '9.68 inches',
-      },
-      operating_system: 'macOS',
-      accessories: [
-        {
-          name: 'Apple Magic Keyboard',
-          price: 99.99,
-        },
-        {
-          name: 'Apple Magic Mouse 2',
-          price: 79.99,
-        },
-        {
-          name: 'USB-C to USB Adapter',
-          price: 19.99,
-        },
-      ],
-    },
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
 
 // ================================================================
 
