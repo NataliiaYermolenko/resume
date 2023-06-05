2e2d85dced92894156e1ae78886a80831e986008
Наталия <pedorg129@gmail.com>
2023-06-05 23:40:19 +0300

    fix layout

--- src/route/index.js
+++ src/route/index.js
@@ -269,347 +269,166 @@ router.get('/work', function (req, res) {
   /
   // ================================================================
 
-  //              ↙ тут вводимо шлях (PATH) до сторінки
-  router.get('/person', function (req, res) {
-    //             ↙ cюди вводимо назву файлу з сontainer
-    res.render('person', {
-      layout: 'person',
-      person: {
-        name: 'Emma Johnson',
-        age: 32,
-        gender: 'Female',
-        address: {
-          street: '123 Main St',
-          city: 'New York',
-          state: 'NY',
-          zip: '10001',
-          country: 'USA',
+  //=================================================================
+
+  // router.get Створює нам один ентпоїнт
+
+  router.get('/facebook', function (req, res) {
+    // res.render генерує нам HTML сторінку
+
+    //            ↙ cюди вводимо назву файлу з сontainer
+    res.render('facebook', {
+      name: 'Facebook',
+      users: [
+        {
+          id: 1,
+          name: 'John Doe',
+          gender: 'Male',
+          age: 30,
+          friends: [
+            {
+              id: 2,
+              name: 'Jane Smith',
+              gender: 'Female',
+              age: 27,
+              mutual_friends: [
+                {
+                  id: 4,
+                  name: 'Samantha Lee',
+                  gender: 'Female',
+                  age: 25,
+                },
+              ],
+            },
+            {
+              id: 5,
+              name: 'Mike Williams',
+              gender: 'Male',
+              age: 28,
+              mutual_friends: [
+                {
+                  id: 7,
+                  name: 'David Kim',
+                  gender: 'Male',
+                  age: 29,
+                },
+              ],
+            },
+          ],
+          groups: [
+            {
+              id: 1,
+              name: 'Hiking Enthusiasts',
+              description:
+                'A group for people who love hiking and the great outdoors',
+              members: [
+                {
+                  id: 8,
+                  name: 'Sarah Johnson',
+                  gender: 'Female',
+                  age: 25,
+                },
+              ],
+            },
+            {
+              id: 2,
+              name: 'Foodies United',
+              description:
+                'A group for food lovers to share recipes and restaurant recommendations',
+              members: [
+                {
+                  id: 5,
+                  name: 'Mike Williams',
+                  gender: 'Male',
+                  age: 28,
+                },
+                {
+                  id: 8,
+                  name: 'Sarah Johnson',
+                  gender: 'Female',
+                  age: 25,
+                },
+              ],
+            },
+          ],
         },
-        education: [
-          {
-            degree: 'Bachelor of Science',
-            major: 'Computer Science',
-            university:
-              'Massachusetts Institute of Technology',
-            graduationYear: 2012,
-          },
-        ],
-        workExperience: [
-          {
-            company: 'Google',
-            title: 'Software Engineer',
-            startDate: '2012-06-01',
-            endDate: '2016-12-31',
-            responsibilities: [
-              'Developed new features for Google Maps',
-              'Worked on improving search algorithms',
-            ],
-            year_founded: 1990,
-            industry: 'Technology',
-            employees: [
-              {
-                name: 'John Smith',
-                position: 'CEO',
-                department: 'Executive',
-                projects: [
-                  {
-                    name: 'Project Alpha',
-                    description:
-                      'Developing new software platform',
-                    status: 'In Progress',
-                    teams: [
-                      {
-                        team_name: 'Awesome Team',
-                        team_leader: {
-                          name: 'John Smith',
-                          title: 'Team Leader',
-                          email: 'john.smith@example.com',
-                        },
-                        team_members: [
-                          {
-                            name: 'Alice Johnson',
-                            title: 'Software Engineer',
-                            email:
-                              'alice.johnson@example.com',
-                            skills: ['Java', 'Python', 'SQL'],
-                            projects: [
-                              {
-                                name: 'Project A',
-                                description:
-                                  'Lorem ipsum dolor sit amet',
-                                technologies: [
-                                  'Java',
-                                  'Spring Framework',
-                                ],
-                                team_members: [
-                                  {
-                                    name: 'Bob Lee',
-                                    title:
-                                      'Software Engineer',
-                                  },
-                                  {
-                                    name: 'Cindy Chen',
-                                    title: 'UI Designer',
-                                  },
-                                ],
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
+        {
+          id: 16,
+          name: 'Amy Lee',
+          gender: 'Female',
+          age: 30,
+          friends: [
+            {
+              id: 15,
+              name: 'Peter Kim',
+              gender: 'Male',
+              age: 32,
+              mutual_friends: [
+                {
+                  id: 17,
+                  name: 'David Chen',
+                  gender: 'Male',
+                  age: 33,
+                },
+                {
+                  id: 19,
+                  name: 'Erica Wong',
+                  gender: 'Female',
+                  age: 29,
+                  mutual_friends: [
+                    {
+                      id: 20,
+                      name: 'Alex Lee',
+                      gender: 'Male',
+                      age: 31,
+                    },
+                  ],
+                },
+              ],
+              groups: [
+                {
+                  id: 3,
+                  name: 'Travel Addicts',
+                  description:
+                    'A group for people who love to travel and explore new places',
+                  members: [
+                    {
+                      id: 22,
+                      name: 'Jackie Chen',
+                      gender: 'Female',
+                      age: 26,
+                    },
+                  ],
+                },
+                {
+                  id: 4,
+                  name: 'Pet Lovers',
+                  description:
+                    'A group for people who love their furry friends',
+                  members: [
+                    {
+                      id: 16,
+                      name: 'Amy Lee',
+                      gender: 'Female',
+                      age: 30,
+                    },
+                    {
+                      id: 25,
+                      name: 'John Smith',
+                      gender: 'Male',
+                      age: 33,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
     })
+    //                  ↑↑ сюди вводимо JSON дані
   })
 
-// ================================================================
-
-
-
-router.get('/js', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('js', {
-    name: 'JavaScript',
-    description:
-      'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
-    history: {
-      year: 1995,
-      founder: 'Brendan Eich',
-    },
-    types: [
-      'Number',
-      'String',
-      'Boolean',
-      'Null',
-      'Undefined',
-      'Symbol',
-      'Object',
-    ],
-    syntax: {
-      variables: ['var', 'let', 'const'],
-      functions: ['function'],
-      conditionals: ['if', 'else', 'switch'],
-      loops: ['for', 'while', 'do-while'],
-      classes: ['class'],
-    },
-    features: [
-      'First-class functions',
-      'Closures',
-      'Dynamic typing',
-      'Prototype-based inheritance',
-      'Asynchronous programming with Promises and async/await',
-      'Modules with import/export statements',
-    ],
-    libraries: [
-      'jQuery',
-      'React',
-      'Angular',
-      'Vue',
-      'Node.js',
-      'Express.js',
-      'Lodash',
-      'Moment.js',
-    ],
-    tools: [
-      'Babel',
-      'Webpack',
-      'ESLint',
-      'Jest',
-      'Mocha',
-      'Chai',
-      'Selenium',
-      'Cypress',
-    ],
-    community: [
-      {
-        name: 'Stack Overflow',
-        type: 'forum',
-      },
-      {
-        name: 'JavaScript Weekly',
-        type: 'newsletter',
-      },
-      {
-        name: 'The Changelog',
-        type: 'podcast',
-      },
-      {
-        name: 'CSS-Tricks',
-        type: 'blog',
-      },
-    ],
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
-//=================================================================
-
-// router.get Створює нам один ентпоїнт
-
-router.get('/facebook', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('facebook', {
-    name: 'Facebook',
-    users: [
-      {
-        id: 1,
-        name: 'John Doe',
-        gender: 'Male',
-        age: 30,
-        friends: [
-          {
-            id: 2,
-            name: 'Jane Smith',
-            gender: 'Female',
-            age: 27,
-            mutual_friends: [
-              {
-                id: 4,
-                name: 'Samantha Lee',
-                gender: 'Female',
-                age: 25,
-              },
-            ],
-          },
-          {
-            id: 5,
-            name: 'Mike Williams',
-            gender: 'Male',
-            age: 28,
-            mutual_friends: [
-              {
-                id: 7,
-                name: 'David Kim',
-                gender: 'Male',
-                age: 29,
-              },
-            ],
-          },
-        ],
-        groups: [
-          {
-            id: 1,
-            name: 'Hiking Enthusiasts',
-            description:
-              'A group for people who love hiking and the great outdoors',
-            members: [
-              {
-                id: 8,
-                name: 'Sarah Johnson',
-                gender: 'Female',
-                age: 25,
-              },
-            ],
-          },
-          {
-            id: 2,
-            name: 'Foodies United',
-            description:
-              'A group for food lovers to share recipes and restaurant recommendations',
-            members: [
-              {
-                id: 5,
-                name: 'Mike Williams',
-                gender: 'Male',
-                age: 28,
-              },
-              {
-                id: 8,
-                name: 'Sarah Johnson',
-                gender: 'Female',
-                age: 25,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        id: 16,
-        name: 'Amy Lee',
-        gender: 'Female',
-        age: 30,
-        friends: [
-          {
-            id: 15,
-            name: 'Peter Kim',
-            gender: 'Male',
-            age: 32,
-            mutual_friends: [
-              {
-                id: 17,
-                name: 'David Chen',
-                gender: 'Male',
-                age: 33,
-              },
-              {
-                id: 19,
-                name: 'Erica Wong',
-                gender: 'Female',
-                age: 29,
-                mutual_friends: [
-                  {
-                    id: 20,
-                    name: 'Alex Lee',
-                    gender: 'Male',
-                    age: 31,
-                  },
-                ],
-              },
-            ],
-            groups: [
-              {
-                id: 3,
-                name: 'Travel Addicts',
-                description:
-                  'A group for people who love to travel and explore new places',
-                members: [
-                  {
-                    id: 22,
-                    name: 'Jackie Chen',
-                    gender: 'Female',
-                    age: 26,
-                  },
-                ],
-              },
-              {
-                id: 4,
-                name: 'Pet Lovers',
-                description:
-                  'A group for people who love their furry friends',
-                members: [
-                  {
-                    id: 16,
-                    name: 'Amy Lee',
-                    gender: 'Female',
-                    age: 30,
-                  },
-                  {
-                    id: 25,
-                    name: 'John Smith',
-                    gender: 'Male',
-                    age: 33,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
-
-// ===============================================================
 //=============================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
